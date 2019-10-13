b76a7b6473ccbd1c926ac6becc18fa42872eddc1
Ligia09 <ligia_babos@yahoo.com>
2019-10-13 14:41:59 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,52 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+    //Tipuri de date
+        String rasa = "Ciobanesc german";
+        String culoare = "Maro";
+        Byte age = 2;
+        float greutate = 23.5f;
+        char gen = 'f';
+        int nivelInteligenta = 1231231231;
+        long nivelEnergie = 435678876546754123L;
+        double nivelNevoieAtentie = 17927486490319504574796468543354645888797970979796554913d;
+
+
+        System.out.println("Despre catelul meu:");
+        System.out.println(rasa);
+        System.out.print(nivelNevoieAtentie);
+        System.out.println(culoare + age + greutate);
+        System.out.println("culoare este" + culoare + "si greutatea" + (age + greutate));
+        System.out.println("culoarea este "+
+                culoare +
+                ",\nage este "+
+                age +
+                " \nsi greutatea este " +
+                greutate);
+
+        int x = 2;
+        int y = 3;
+        int sum  = x + y;
+        float average = sum + 2f;
+
+ //       x+=y; // x = x + y;
+ //       x-=y; // x = x - y;
+ //       x+=2; // x = x + 2;
+
+
+        System.out.println(average);
+
+
+
+
+
+
+
+
+
+
+
+    }// write your code here
+}
